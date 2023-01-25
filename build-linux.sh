8cb80d5d33b1d0e2a6ba19b75a0b8caeb7015ba0
openaudible <racer@torguard.tg>
2023-01-24 21:14:43 -0800

    remove_frontend

--- build-linux.sh
+++ build-linux.sh
@@ -12,7 +12,6 @@ if [ ! -e $FFMPEG_TARBALL ]
 then
 	curl -s -L -O $FFMPEG_TARBALL_URL
 fi
-ARCH="x86_64"
 
 : ${ARCH?}
 
@@ -85,7 +84,7 @@ FFMPEG_CONFIGURE_FLAGS+=(--prefix=$PREFIX)
 do_svn_checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame_svn
   cd lame_svn
     echo "Compiling lame: prefix $PREFIX"
-    ./configure --enable-nasm --disable-decoder --prefix=$PREFIX --enable-static --disable-shared
+    ./configure --enable-nasm --disable-decoder --prefix=$PREFIX --enable-static --disable-shared --disable-frontend
     make -j8
     make install
   cd ..
