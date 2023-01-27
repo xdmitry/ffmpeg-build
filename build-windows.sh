1413c35c33f7d0633445b00f959112ce25ba51fa
openaudible <racer@torguard.tg>
2023-01-27 11:25:43 -0800

    misc x

--- build-windows.sh
+++ build-windows.sh
@@ -14,24 +14,36 @@ if [ ! -e $FFMPEG_TARBALL ]
 then
 	curl -s -L -O $FFMPEG_TARBALL_URL
 fi
+ARCH=x86_64
+host=x86_64-w64-mingw32
 
 : ${ARCH?}
 
 OUTPUT_DIR=artifacts/ffmpeg-$FFMPEG_VERSION-audio-$ARCH-w64-mingw32
 
 BUILD_DIR=$(mktemp -d -p $(pwd) build.XXXXXXXX)
-trap 'rm -rf $BUILD_DIR' EXIT
+# TODO: PUT BACK IN trap 'rm -rf $BUILD_DIR' EXIT
 
 cd $BUILD_DIR
 tar --strip-components=1 -xf $BASE_DIR/$FFMPEG_TARBALL
 
+
+PREFIX=$BASE_DIR/$OUTPUT_DIR
+CF="-static -static-libgcc -static-libstdc++ -I$PREFIX/include"
+# CF="'$CF'"
+echo "CF=$CF"
+
 FFMPEG_CONFIGURE_FLAGS+=(
-    --prefix=$BASE_DIR/$OUTPUT_DIR
-    --extra-cflags='-static -static-libgcc -static-libstdc++'
+    --prefix=$PREFIX
+    --extra-cflags='-static -static-libgcc -static-libstdc++ -I/data/dev/ff/ffmpeg-build/artifacts/ffmpeg-5.1.2-audio-x86_64-w64-mingw32/include'
+    --extra-ldflags=-L$PREFIX/lib
     --target-os=mingw32
     --arch=$ARCH
     --cross-prefix=$ARCH-w64-mingw32-
 )
+
+echo "test ./configure ${FFMPEG_CONFIGURE_FLAGS[@]}"
+
 
 # Build lame
 PREFIX=$BASE_DIR/$OUTPUT_DIR
@@ -41,18 +53,29 @@ FFMPEG_CONFIGURE_FLAGS+=(--prefix=$PREFIX)
 do_svn_checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame_svn
   cd lame_svn
     echo "Compiling lame: prefix $PREFIX"
-    ./configure --enable-nasm --disable-decoder --prefix=$PREFIX --enable-static --disable-shared --disable-frontend
+    ./configure --disable-decoder --prefix=$PREFIX --enable-static --disable-shared --host=$host
     make -j8
     make install
   cd ..
-echo "compiled LAME... "
+echo "compiled LAME... $PREFIX "
 
+find $PREFIX 
+
+echo "./configure ${FFMPEG_CONFIGURE_FLAGS[@]}"
+PL=$PREFIX/lib
+ls -la $PL
+file $PL/libmp3lame.a
+pwd && ls
 
 FFMPEG_CONFIGURE_FLAGS+=(--extra-cflags="-I$PREFIX/include")
-FFMPEG_CONFIGURE_FLAGS+=(--extra-ldflags="-L$PREFIX/lib")
+# FFMPEG_CONFIGURE_FLAGS+=(--extra-ldflags="-L$PREFIX/lib")
+# echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+
+# LD_LIBRARY_PATH+=" $PREFIX/lib"
 
 echo "configure ffmpeg: ${FFMPEG_CONFIGURE_FLAGS[@]}"
 
+set -x
 
 ./configure "${FFMPEG_CONFIGURE_FLAGS[@]}"
 make
